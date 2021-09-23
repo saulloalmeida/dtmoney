2660187a085f0c3a7a9719d78d12f592c8ddad5f
Saullo Almeida <saullorhamon@gmail.com>
2021-09-23 18:41:09 -0300

    iniciando projeto dtmoney
    *incluido configurações de css, fontes, icones e svg's

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+    background: var(--blue);
+`
+
+export const Content = styled.div`
+    max-width: 1120px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 1rem 10rem;
+`
